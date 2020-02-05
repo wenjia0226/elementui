c06d8b8a9a75f14a70ce1690389689a2882be497
wenjia0226 <1320073909@qq.com>
2020-02-05 17:31:55 +0800

    schoolanalysis

--- src/components/analysis/class.vue
+++ src/components/analysis/class.vue
@@ -1,11 +1,187 @@
 <template>
-    <div>class</div>
+    <div>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>数据分析</el-breadcrumb-item>
+          <el-breadcrumb-item>班级概况</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-card>
+        <el-row :gutter="20">
+            <el-col :span="4">
+                 <el-cascader :options="options" v-model="stu_cat" :props="cateProps" @change="handleChange" clearable></el-cascader>
+            </el-col>
+            <el-col :span="6">
+               <el-button type="primary" @click="showClass">查看班级概况</el-button>
+            </el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+           <el-col :span="12">
+             <div ref="left" style="width: 600px;height:400px;;"></div>
+           </el-col>
+           <el-col :span="12">
+             <div ref="right" style="width: 600px;height:400px;;"></div>
+           </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span = "12" :offset="6">
+             <div ref="double" style="width: 600px;height:400px;"></div>
+          </el-col>
+        </el-row>
+        </el-card>
+    </div>
 </template>
 <script>
+  import echarts from 'echarts'
+  import axios from 'axios'
 export default {
-    
+  created() {
+    this.token = window.sessionStorage.getItem('token');
+     this.getOPtions();
+  },
+    data() {
+        return {
+          token: '',
+          schoolId: '',
+           schooloptions: [],
+            value: '',
+            leftOption: [],
+            rightOption: [],
+            doubleOption: [],
+            leftLegend: [],
+            rightLegend: [],
+            doubleLegend: [],
+            stu_cat: [],
+            cateProps: {
+            label: 'name', //看到的是哪个属性
+            value: 'id', // 选中的是谁的值
+            children: 'children' //哪个属性实现父子节点嵌套
+            },
+            options: [],
+            classId: '',
+            schoolId: ''
+        }
+    },
+
+  methods: {
+    drawLine(id,lengend, option) {
+      if(id == 'left') {
+         var myChart = echarts.init(this.$refs.left);
+      }else if(id == 'right') {
+        var myChart = echarts.init(this.$refs.right);
+      }else{
+         var myChart = echarts.init(this.$refs.double);
+      }
+      console.log(id, lengend, option)
+       this.option = {
+          title: {
+              text: '',
+              subtext: '',
+              left: 'center'
+          },
+          tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: lengend
+              // data: ['左眼良好数量', '左眼轻度不良数量', '左眼中度不良数量', '左眼重度不良数量'],
+              },
+          series: [
+              {
+                  name: '访问来源',
+                  type: 'pie',
+                  radius: '80%',
+                  center: ['50%', '60%'],
+                  data: option,
+                  // data: [
+                  //     {value: 7, name: '左眼良好数量'},
+                  //     {value: 10, name: '左眼轻度不良数量'},
+                  //     {value: 5, name: '左眼中度不良数量'},
+                  //     {value: 1, name: '左眼重度不良数量'}
+                  // ],
+                  emphasis: {
+                      itemStyle: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      };
+      myChart.setOption(this.option)
+     },
+     //改变选中数据
+     handleChangeSchool(val) {
+       this.schoolId = val;
+     },
+    //获取学校近视眼概况
+    showClass() {
+      this.leftLegend = [];
+      let param = new URLSearchParams();
+      param.append('token', this.token);
+      param.append('classId', this.classId);
+      axios({
+        method: 'post',
+        url:'/api/classStatistics',
+        data: param
+      }).then(this.getClassAnalysisSucc.bind(this)).catch(this.handleGetClassAnalysisErr.bind(this))
+    },
+    getClassAnalysisSucc(res) {
+      if(res.status !== 200) return;
+      res.data.data ? res = res.data.data: '';
+      this.leftOption = res[0];
+      this.rightOption = res[1];
+      this.doubleOption = res[2];
+      this.leftOption.forEach((item, index) => {
+        this.leftLegend.push(item.name);
+      })
+      this.rightOption.forEach((item, index) => {
+        this.rightLegend.push(item.name);
+      })
+      this.doubleOption.forEach((item, index) => {
+        this.doubleLegend.push(item.name);
+      })
+      console.log(this.leftOption, this.rightOption, this.doubleOption)
+      console.log(this.leftLegend, this.rightLegend, this.doubleLegend)
+        this.drawLine('left', this.leftLegend, this.leftOption);
+        this.drawLine('right', this.rightLegend, this.rightOption);
+        this.drawLine('double', this.doubleLegend,this.doubleOption);
+
+
+
+
+    },
+    handleGetClassAnalysisErr(err) {
+      console.log(err)
+    },
+    //获取级联选择器中的数据
+    getOPtions() {
+        let param = new URLSearchParams();
+        param.append('token', this.token);
+        axios({
+            method: 'post',
+            url: '/api/cascade1',
+            data: param
+        }).then(this.handleGetOptionSucc.bind(this)).catch(this.handleGetOptionErr.bind(this))
+    },
+    handleGetOptionSucc (res) {
+
+        if(res.status !==200) return this.$message.error('获取级联数据失败');
+        this.options =  res.data.data;
+    },
+    handleGetOptionErr(err) {
+        console.log(err)
+    },
+    handleChange() {
+       this.schoolId = this.stu_cat[0];
+       this.classId = this.stu_cat[1];
+    },
+   }
 }
 </script>
-<style lang="less">
+<style  scoped>
 
 </style>
--- src/components/analysis/school.vue
+++ src/components/analysis/school.vue
@@ -1,43 +1,183 @@
 <template>
     <div>
-        <el-card>
-            <div ref="main" style="width: 600px;height:400px;"></div>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>数据分析</el-breadcrumb-item>
+          <el-breadcrumb-item>校园概况</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-card>
+        <el-row :gutter="20">
+            <el-col :span="12"></el-col>
+            <el-col :span="4">
+                 <el-select v-model="value" placeholder="请选择" @change="handleChangeSchool">
+                    <el-option
+                      v-for="item in schooloptions"
+                      :key="item.id"
+                      :label="item.name"
+                      :value="item.id">
+                    </el-option>
+                 </el-select>
+            </el-col>
+            <el-col :span="6">
+               <el-button type="primary" @click="showSchool">查看校园概况</el-button>
+            </el-col>
+        </el-row>
+        <el-row style="margin: 10px 0">
+           <el-col :span="12">
+             <div ref="left" style="width: 600px;height:400px;;"></div>
+           </el-col>
+           <el-col :span="12">
+             <div ref="right" style="width: 600px;height:400px;;"></div>
+           </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span = "12" :offset="6">
+             <div ref="double" style="width: 600px;height:400px;"></div>
+          </el-col>
+        </el-row>
+
         </el-card>
     </div>
 </template>
 <script>
-import echarts from 'echarts';
+  import echarts from 'echarts'
+  import axios from 'axios'
 export default {
-    data() {
-        return {}
-    },
-    //dom渲染完毕后
-     mounted(){
-    this.drawLine();
+  created() {
+    this.token = window.sessionStorage.getItem('token');
+    this.getSchoolList();
   },
+    data() {
+        return {
+          token: '',
+          schoolId: '',
+           schooloptions: [],
+            value: '',
+            leftOption: [],
+            rightOption: [],
+            doubleOption: [],
+            leftLegend: [],
+            rightLegend: [],
+            doubleLegend: []
+        }
+    },
+
   methods: {
-    drawLine(){
-        // 基于准备好的dom，初始化echarts实例
-       
-        let myChart = this.$echarts.init(this.$refs.mian)
-        // 绘制图表
-        myChart.setOption({
-            title: { text: '在Vue中使用echarts' },
-            tooltip: {},
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        });
-    }
-  }
+    drawLine(id,lengend, option) {
+      if(id == 'left') {
+         var myChart = echarts.init(this.$refs.left);
+      }else if(id == 'right') {
+        var myChart = echarts.init(this.$refs.right);
+      }else{
+         var myChart = echarts.init(this.$refs.double);
+      }
+      console.log(id, lengend, option)
+       this.option = {
+          title: {
+              text: '',
+              subtext: '',
+              left: 'center'
+          },
+          tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          legend: {
+              orient: 'vertical',
+              left: 'left',
+              data: lengend
+              // data: ['左眼良好数量', '左眼轻度不良数量', '左眼中度不良数量', '左眼重度不良数量'],
+              },
+          series: [
+              {
+                  name: '访问来源',
+                  type: 'pie',
+                  radius: '80%',
+                  center: ['50%', '60%'],
+                  data: option,
+                  // data: [
+                  //     {value: 7, name: '左眼良好数量'},
+                  //     {value: 10, name: '左眼轻度不良数量'},
+                  //     {value: 5, name: '左眼中度不良数量'},
+                  //     {value: 1, name: '左眼重度不良数量'}
+                  // ],
+                  emphasis: {
+                      itemStyle: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      };
+      myChart.setOption(this.option)
+     },
+     //获取学校下拉列表
+     getSchoolList() {
+       var param = new URLSearchParams();
+       param.append('token', this.token);
+       axios({
+         method:'post',
+         url: '/api/schoolList',
+         data: param
+       }).then(this.handleGetSchoolLisSucc.bind(this)).catch(this.handleGetSchoolListErr.bind(this))
+     },
+     handleGetSchoolLisSucc(res) {
+       if(res.status !== 200) return;
+       this.schooloptions = res.data.data;
+
+     },
+     handleGetSchoolListErr(err) {
+       console.log(err)
+     },
+     //改变选中数据
+     handleChangeSchool(val) {
+       this.schoolId = val;
+     },
+    //获取学校近视眼概况
+    showSchool() {
+      this.leftLegend = [];
+      let param = new URLSearchParams();
+      param.append('token', this.token);
+      param.append('schoolId', this.schoolId);
+      axios({
+        method: 'post',
+        url:'/api/schoolStatistics',
+        data: param
+      }).then(this.getSchoolAnalysisSucc.bind(this)).catch(this.handleGetSchoolAnalysisErr.bind(this))
+    },
+    getSchoolAnalysisSucc(res) {
+      if(res.status !== 200) return;
+      res.data.data ? res = res.data.data: '';
+      this.leftOption = res[0];
+      this.rightOption = res[1];
+      this.doubleOption = res[2];
+      this.leftOption.forEach((item, index) => {
+        this.leftLegend.push(item.name);
+      })
+      this.rightOption.forEach((item, index) => {
+        this.rightLegend.push(item.name);
+      })
+      this.doubleOption.forEach((item, index) => {
+        this.doubleLegend.push(item.name);
+      })
+      console.log(this.leftOption, this.rightOption, this.doubleOption)
+      console.log(this.leftLegend, this.rightLegend, this.doubleLegend)
+        this.drawLine('left', this.leftLegend, this.leftOption);
+        this.drawLine('right', this.rightLegend, this.rightOption);
+        this.drawLine('double', this.doubleLegend,this.doubleOption);
+
+
+
+
+    },
+    handleGetSchoolAnalysisErr(err) {
+      console.log(err)
+    },
+   }
 }
 </script>
-<style lang="less">
+<style  scoped>
 
 </style>
