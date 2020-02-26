d70b8dff503895c6cdcc511ad1e07b30bfe69322
wenjia0226 <1320073909@qq.com>
2020-02-26 15:26:06 +0800

    school

--- src/components/analysis/school.vue
+++ src/components/analysis/school.vue
@@ -22,20 +22,57 @@
                <el-button type="primary" @click="showSchool">查看校园概况</el-button>
             </el-col>
         </el-row>
-        <el-row style="margin: 30px 0" >
+        <!-- 校园 -->
+        <el-row >
+          <el-col :span="6" :offset="9" style="padding: 20px; fong-weight: bold;font-size: 34px;" v-show="this.totaldoubleLegend.length">校园概况</el-col>
+        </el-row>
+        <el-row style="margin: 30px 0" v-show="this.totaldoubleLegend.length" >
            <el-col :span="12">
-             <div ref="left" style="width: 600px;height:400px; margin: 0 auto"></div>
+             <div ref="totalleft" style="width: 600px;height:400px; margin: 0 auto"></div>
            </el-col>
            <el-col :span="12">
-             <div ref="right" style="width: 600px;height:400px; margin: 0 auto"></div>
+             <div ref="totalright" style="width: 600px;height:400px; margin: 0 auto"></div>
            </el-col>
         </el-row>
-        <el-row type="flex" style="margin: 40px 0">
+        <el-row type="flex" style="margin: 40px 0" v-show="this.totaldoubleLegend.length">
           <el-col :span = "12" :offset="6" >
-             <div ref="double" style="width: 600px;height:400px;margin: 0 auto"></div>
+             <div ref="totaldouble" style="width: 600px;height:400px;margin: 0 auto"></div>
+        </el-col>
+        </el-row>
+        <!-- 实验班 -->
+        <el-row v-show="this.totaldoubleLegend.length">
+          <el-col :span="6" :offset="9"  style="padding: 20px; fong-weight: bold;font-size: 34px;">实验班概况</el-col>
+        </el-row>
+        <el-row style="margin: 30px 0" v-show="this.totaldoubleLegend.length">
+           <el-col :span="12">
+             <div ref="testleft" style="width: 600px;height:400px; margin: 0 auto"></div>
+           </el-col>
+           <el-col :span="12">
+             <div ref="testright" style="width: 600px;height:400px; margin: 0 auto"></div>
+           </el-col>
+        </el-row>
+        <el-row type="flex" style="margin: 40px 0" v-show="this.totaldoubleLegend.length">
+          <el-col :span = "12" :offset="6" >
+             <div ref="testdouble" style="width: 600px;height:400px;margin: 0 auto"></div>
           </el-col>
         </el-row>
-
+       <!-- 非实验班 -->
+        <el-row v-show="this.totaldoubleLegend.length">
+          <el-col :span="6" :offset="9"  style="padding: 20px; fong-weight: bold;font-size: 34px;">非实验班概况</el-col>
+        </el-row>
+        <el-row style="margin: 30px 0" v-show="this.totaldoubleLegend.length">
+           <el-col :span="12">
+             <div ref="normalleft" style="width: 600px;height:400px; margin: 0 auto"></div>
+           </el-col>
+           <el-col :span="12">
+             <div ref="normalright" style="width: 600px;height:400px; margin: 0 auto"></div>
+           </el-col>
+        </el-row>
+        <el-row type="flex" style="margin: 40px 0" v-show="this.totaldoubleLegend.length">
+          <el-col :span = "12" :offset="6" >
+             <div ref="normaldouble" style="width: 600px;height:400px;margin: 0 auto"></div>
+          </el-col>
+        </el-row>
         </el-card>
     </div>
 </template>
@@ -53,23 +90,48 @@ export default {
           schoolId: '',
            schooloptions: [],
             value: '',
-            leftOption: [],
-            rightOption: [],
-            doubleOption: [],
-            leftLegend: [],
-            rightLegend: [],
-            doubleLegend: []
+            normalleftOption: [],
+            normalrightOption: [],
+            normaldoubleOption: [],
+            normalleftLegend: [],
+            normalrightLegend: [],
+            normaldoubleLegend: [],
+            testleftOption: [],
+            testrightOption: [],
+            testdoubleOption: [],
+            testleftLegend: [],
+            testrightLegend: [],
+            testdoubleLegend: [],
+            totalleftOption: [],
+            totalrightOption: [],
+            totaldoubleOption: [],
+            totalleftLegend: [],
+            totalrightLegend: [],
+            totaldoubleLegend: [],
         }
     },
 
   methods: {
     drawLine(id,lengend, option, text) {
-      if(id == 'left') {
-         var myChart = echarts.init(this.$refs.left);
-      }else if(id == 'right') {
-        var myChart = echarts.init(this.$refs.right);
-      }else{
-         var myChart = echarts.init(this.$refs.double);
+      console.log(id, 'normlleft' )
+      if(id == 'normalleft') {
+         var myChart = echarts.init(this.$refs.normalleft);
+      }else if(id == 'normalright') {
+        var myChart = echarts.init(this.$refs.normalright);
+      }else if(id == 'normaldouble'){
+         var myChart = echarts.init(this.$refs.normaldouble);
+      }else if (id == 'testleft') {
+         var myChart = echarts.init(this.$refs.testleft);
+      }else if(id == 'testright') {
+        var myChart = echarts.init(this.$refs.testright);
+      }else if(id == 'testdouble'){
+         var myChart = echarts.init(this.$refs.testdouble);
+      }else if (id == 'totalleft') {
+         var myChart = echarts.init(this.$refs.totalleft);
+      }else if(id == 'totalright') {
+        var myChart = echarts.init(this.$refs.totalright);
+      }else if(id == 'totaldouble'){
+         var myChart = echarts.init(this.$refs.totaldouble);
       }
        this.option = {
           title: {
@@ -118,6 +180,7 @@ export default {
               }
           ]
       };
+      console.log(myChart, this.option)
       myChart.setOption(this.option)
      },
      //获取学校下拉列表
@@ -164,26 +227,58 @@ export default {
       } else if(res.data.status == 200) {
           res.data.data ? res = res.data.data: '';
           console.log(res)
-          this.leftOption = res[0];
-          this.rightOption = res[1];
-          this.doubleOption = res[2];
-          this.leftOption.forEach((item, index) => {
-            this.leftLegend.push(item.name);
+          let normal = res.normalData;
+          let test = res.testData;
+          let total = res.totalData;
+          //非实验班
+          this.normalleftOption = normal[0];
+          this.normalrightOption = normal[1];
+          this.normaldoubleOption = normal[2];
+          this.normalleftOption.forEach((item, index) => {
+            this.normalleftLegend.push(item.name);
           })
-          this.rightOption.forEach((item, index) => {
-            this.rightLegend.push(item.name);
+          this.normalrightOption.forEach((item, index) => {
+            this.normalrightLegend.push(item.name);
           })
-          this.doubleOption.forEach((item, index) => {
-            this.doubleLegend.push(item.name);
+          this.normaldoubleOption.forEach((item, index) => {
+            this.normaldoubleLegend.push(item.name);
           })
-         }
-        this.drawLine('left', this.leftLegend, this.leftOption, '左眼概况');
-        this.drawLine('right', this.rightLegend, this.rightOption,'右眼概况');
-        this.drawLine('double', this.doubleLegend,this.doubleOption, '双眼概况');
-
-
-
-
+        this.drawLine('normalleft', this.normalleftLegend, this.normalleftOption, '左眼概况');
+        this.drawLine('normalright', this.normalrightLegend, this.normalrightOption,'右眼概况');
+        this.drawLine('normaldouble', this.normaldoubleLegend,this.normaldoubleOption, '双眼概况');
+        //实验班
+        this.testleftOption = test[0];
+        this.testrightOption = test[1];
+        this.testdoubleOption = test[2];
+        this.testleftOption.forEach((item, index) => {
+          this.testleftLegend.push(item.name);
+        })
+        this.testrightOption.forEach((item, index) => {
+          this.testrightLegend.push(item.name);
+        })
+        this.testdoubleOption.forEach((item, index) => {
+          this.testdoubleLegend.push(item.name);
+        })
+        this.drawLine('testleft', this.testleftLegend, this.testleftOption, '左眼概况');
+        this.drawLine('testright', this.testrightLegend, this.testrightOption,'右眼概况');
+        this.drawLine('testdouble', this.testdoubleLegend,this.testdoubleOption, '双眼概况');
+        //总共
+        this.totalleftOption = total[0];
+          this.totalrightOption = total[1];
+          this.totaldoubleOption = total[2];
+          this.totalleftOption.forEach((item, index) => {
+            this.totalleftLegend.push(item.name);
+          })
+          this.totalrightOption.forEach((item, index) => {
+            this.totalrightLegend.push(item.name);
+          })
+          this.totaldoubleOption.forEach((item, index) => {
+            this.totaldoubleLegend.push(item.name);
+          })
+        this.drawLine('totalleft', this.totalleftLegend, this.totalleftOption, '左眼概况');
+        this.drawLine('totalright', this.totalrightLegend, this.totalrightOption,'右眼概况');
+        this.drawLine('totaldouble', this.totaldoubleLegend,this.totaldoubleOption, '双眼概况');
+      }
     },
     handleGetSchoolAnalysisErr(err) {
       console.log(err)
