9b5513c3373810f7e84cfa601a1207ef78194f6c
wenjia0226 <1320073909@qq.com>
2020-08-04 16:28:02 +0800

    1

--- config/index.js
+++ config/index.js
@@ -12,9 +12,9 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/lightspace': {
-        target: 'http://47.104.222.22:8080/',
-       //target: 'https://www.guangliangkongjian.com',
-        target: 'http://192.168.100.199:8080/',
+        //target: 'http://47.104.222.22:8080/',
+        target: 'https://www.guangliangkongjian.com',
+        //target: 'http://192.168.100.199:8080/',
         changeOrigin: true,  //允许跨域
       }
     },
